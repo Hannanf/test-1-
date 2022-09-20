7daa332f41a1ad88aa4b15eb5d2db4602930aade
HNN-WORK <HNN-WRK@Hannans-MacBook-Pro.local>
2022-09-20 00:52:36 -0400

    latest

--- PyPoll.py
+++ PyPoll.py
@@ -36,14 +36,19 @@ with open (file_to_load) as election_data:
         vote_percentage = float(votes)/float(total_votes) *100
 
         
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes 
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
     
-            
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 
-    
